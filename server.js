e042a8a37d3573ed36db920f2c28e9092c8e6ff4
KibbieKatt <224451654+KibbieKatt@users.noreply.github.com>
2026-03-08 18:58:10 -0400

    Add cache and mutex to watch endpoint

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import { cleanEnv, num, str } from 'envalid';
 import express from'express';
 import which from 'which';
 import YTDlpWrap from 'yt-dlp-wrap';
+import NodeCache from 'node-cache';
+import { Mutex } from 'async-mutex';
+import { proxyFetch } from './lib/proxyFetch.js';
 
 const env = cleanEnv(process.env, {
   PORT: num({ default: 3000 }),
@@ -14,6 +17,11 @@ const youtubedl = new YTDlpWrap.default(ytdlpBin);
 
 const app = express();
 
+// Cache with default ttl for playlists
+const cache = new NodeCache({ stdTTL: 18000, checkperiod: 30 });
+// Use a mutex to avoid racing fetches
+const cacheMutex = new Mutex();
+
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
@@ -21,20 +29,57 @@ app.get('/health', (req, res) => {
 app.get('/watch', async (req, res) => {
   const videoID = req.query.v;
   if (/^([\w-]{11})$/.test(videoID)) {
-    // Get m3u8 from yt-dlp
-    return youtubedl.execPromise([
-      "-g",
-      "--format-sort",
-      "proto:m3u8",
-      `https://www.youtube.com/watch?v=${videoID}`,
-    ]).then(async (stdout) => {
-      const url = stdout.trim();
-      if (!url.endsWith(".m3u8")) {
-        return Promise.reject({
-          message: "YouTube returned a non-m3u8 url: " + url
+    // Use mutex to make cache check/load atomic.
+    // Intended to block raced clients until the first completes and
+    // primes the cache for the second
+    return cacheMutex.runExclusive(async () => {
+      const cachedResponse = cache.get(videoID);
+
+      if (cachedResponse) {
+        console.log(`Serving from cache: ${videoID}`);
+
+        return cachedResponse;
+      } else {
+        console.log(`New request for: ${videoID}`);
+
+        // Get m3u8 from yt-dlp
+        return youtubedl.execPromise([
+          "-g",
+          "--format-sort",
+          "proto:m3u8",
+          `https://www.youtube.com/watch?v=${videoID}`,
+        ]).then(async (stdout) => {
+          const url = stdout.trim();
+          if (!url.endsWith(".m3u8")) {
+            return Promise.reject({
+              message: "YouTube returned a non-m3u8 url: " + url
+            });
+          }
+
+          // Proxy m3u8 and rewrite URLs before caching and responding
+          return proxyFetch(url)
+            .then(async (response) => {
+              if (!response.ok) {
+                return res.status(response.status).json({
+                  error: response.statusText,
+                  status: response.status
+                });
+              }
+              // Tecnically thix promise leaks rejections but I'm choosing to not think about it
+              const playlistData = await response.text();
+              cache.set(videoID, playlistData);
+
+              return playlistData;
+            });
         });
       }
-      res.status(200).send(url);
+    }).then(playlistData => {
+      // Separate response code outside mutex to avoid blocking on data transfer
+      res.set({
+        "Content-Type": "application/vnd.apple.mpegurl",
+        "Cache-Control": "public, max-age=600" // 10 minutes
+      });
+      return res.status(200).send(playlistData);
     }).catch(error => {
       // yt-dlp command returned an error
       const message = error.message.match(/(?<=Stderr:\n)(.*)/) || error.message
@@ -49,6 +94,14 @@ app.get('/watch', async (req, res) => {
       error: "Invalid video ID",
     });
   }
+});
+
+// Catchall error
+app.use((err, req, res, next) => {
+  console.error('Uncaught error:', err.stack);
+  res.status(500).json({
+    error: "Internal server error",
+  });
 });
 
 app.listen(env.PORT, (err) => {
